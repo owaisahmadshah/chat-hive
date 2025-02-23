118b1cdee92280031aff829b598f674dd1873331
Owais <owaisahmadqureshi019@gmail.com>
2025-02-23 19:14:47 +0500

    feat(middlewares): Added some common express middlwares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
 
 import logger from "./logger.js"
 
@@ -7,6 +8,10 @@ const app = express()
 
 const morganFormat = ":method :url :status :response-time ms"
 
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
 app.use(
   morgan(morganFormat, {
     stream: {
