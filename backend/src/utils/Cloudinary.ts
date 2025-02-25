fa0a9632dc058e3952edf60277d06d21fb816c5c
Owais <owaisahmadqureshi019@gmail.com>
2025-02-26 00:25:33 +0500

    fix(cloudinary) remove an extra line from cloudinary.

--- backend/src/utils/Cloudinary.ts
+++ backend/src/utils/Cloudinary.ts
@@ -2,8 +2,6 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 import logger from "../logger.js"
 
-cloudinary.config()
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
