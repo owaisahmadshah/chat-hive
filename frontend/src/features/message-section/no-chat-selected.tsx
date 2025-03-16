cfe86daeedaaa4723383c93e246692a08523f9fe
Owais <owaisahmadqureshi019@gmail.com>
2025-03-17 02:38:10 +0500

    feat(no-chat-selected) Added page if not chat is selected it will display instead.

---
+++ frontend/src/features/message-section/no-chat-selected.tsx
@@ -0,0 +1,9 @@
+const NoChatSelectedLandingPage = () => {
+  return (
+    <div className="w-[75vw] h-[99vh] flex items-center justify-center bg-background text-foreground">
+      <p className="text-lg font-medium">You haven't created or selected any chat.</p>
+    </div>
+  )
+}
+
+export default NoChatSelectedLandingPage
