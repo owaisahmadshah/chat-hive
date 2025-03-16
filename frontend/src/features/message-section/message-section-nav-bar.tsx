6e64e0c87d8ec184268100697c31ce6b3e588296
Owais <owaisahmadqureshi019@gmail.com>
2025-03-17 04:09:57 +0500

    feat(message-section-nav-bar) added messages section navbar which displays username and userImage

---
+++ frontend/src/features/message-section/message-section-nav-bar.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useSelector } from "react-redux"
+
+import { RootState } from "@/store/store"
+
+const MessageSectionNavBar = () => {
+
+  const { selectedChat, selectedChatUser } = useSelector((state: RootState) => state.chats)
+
+  const handleClickOnSelectedChatProfile = () => {
+    // TODO 
+  }
+
+  return (
+    <ul className="w-[100%] h-[15vh] flex gap-5 items-center p-5 bg-background border-b border-r">
+      <li className=" cursor-pointer" onClick={handleClickOnSelectedChatProfile}>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </li>
+      <li className="font-bold text-lg cursor-pointer" onClick={handleClickOnSelectedChatProfile}>{selectedChatUser?.email}</li>
+    </ul>
+  )
+}
+
+export default MessageSectionNavBar
