5dc334a82dc6013ee18abdbd4f40b3278c0b6a18
Owais <owaisahmadqureshi019@gmail.com>
2025-03-16 18:01:09 +0500

    feat(useGetUserId, axios instance) Added a hook that will fetch user data and create axios instance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,13 @@ import { SignedIn, SignedOut } from "@clerk/clerk-react"
 
 import ContinueWithGoogle from "@/features/auth/continue-with-google-landing-page"
 import ChatSection from "@/features/chat-section/chat-section"
+import useGetUserId from "./hooks/useGetUserId"
 
 function App() {
+
+  //* This will get the user data and userId which is crucial for making api requests
+  useGetUserId()
+
   return (<>
     <SignedOut>
       <ContinueWithGoogle />
---
+++ frontend/src/hooks/useGetUserId.ts
@@ -0,0 +1,64 @@
+import { useAuth, useUser } from "@clerk/clerk-react"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import axios from "axios"
+
+import { setUser } from "@/store/slices/user"
+import { User } from "@/types/user-interface"
+import api from "@/lib/axiosInstance"
+
+function useGetUserId() {
+  const { isSignedIn, user } = useUser()
+  const { getToken } = useAuth()
+
+  const dispatch = useDispatch()
+
+  // When user opens website we will fetch his userId and details
+  useEffect(() => {
+    let isMounted = true
+
+    if (isSignedIn && user?.id) {
+      ;(async () => {
+        try {
+          const token = await getToken()
+
+          // Stop if component is unmounted
+          if (!isMounted) {
+            return
+          }
+
+          const { data } = await api.post(
+            "/v1/webhook/user",
+            { clerkId: user.id },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+
+          if (isMounted && data.statusCode === 200) {
+            const payload: User = {
+              email: data.data.email,
+              userId: data.data._id,
+              fullName: data.data.fullName,
+              imageUrl: data.data.imageUrl,
+              isLoading: true,
+            }
+            dispatch(setUser(payload))
+          }
+        } catch (error) {
+          console.error("Error getting userId", error)
+          if (axios.isAxiosError(error)) {
+            console.error("Axios error details:", error.response?.data)
+          }
+        }
+      })()
+    }
+    return () => {
+      isMounted = false
+    }
+  }, [isSignedIn, user?.id, getToken])
+}
+
+export default useGetUserId
