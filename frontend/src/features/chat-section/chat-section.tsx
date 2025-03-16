fcdee575b4765f82e2be2970f3da32eb818bcd8d
Owais <owaisahmadqureshi019@gmail.com>
2025-03-17 01:52:25 +0500

    feat(chat-section) Updated chat section UI and also added border to the chat-section-navbar

---
+++ frontend/src/features/chat-section/chat-section.tsx
@@ -0,0 +1,16 @@
+import ChatNavbar from "@/features/chat-section/chat-section-nav-bar"
+import useGetUserChatsAndMessages from "@/hooks/useGetUserChatsAndMessages"
+import Chats from "./chats"
+
+const ChatSection = () => {
+  useGetUserChatsAndMessages()
+
+  return (
+    <div className="flex flex-col bg-background box-border max-w-[25%] max-h-[100vh] min-h-[100vh]">
+      <ChatNavbar />
+      <Chats />
+    </div>
+  )
+}
+
+export default ChatSection
