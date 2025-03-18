2ca2a0537f879dae57c67cba87b0f7e085d7640b
Owais <owaisahmadqureshi019@gmail.com>
2025-03-18 13:55:11 +0500

    feat(selectedMessageOptions) added selected message options component

---
+++ frontend/src/features/message-section/selected-message-drop-down-options.tsx
@@ -0,0 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import ArrowDown from "./arrow-down"
+import { Message } from "@/types/message-interface"
+
+function MessageDropDownOptions({ selectedMessage }: { selectedMessage: Message }) {
+  const handleSelectedMessageReply = () => {
+    // TODO handle selected message delete
+  }
+
+  const handleSelectedMessageCopy = () => {
+    // TODO handle selected message copy
+  }
+
+  const handleSelectedMessagePin = () => {
+    // TODO handle selected message pin
+  }
+
+  const handleSelectedMessageDelete = () => {
+    // TODO handle selected message delete
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <span className=" opacity-0 hover:opacity-100 cursor-pointer">
+          <ArrowDown />
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={handleSelectedMessageReply}>Reply</DropdownMenuItem>
+        <DropdownMenuItem onClick={handleSelectedMessageCopy}>Copy</DropdownMenuItem>
+        <DropdownMenuItem onClick={handleSelectedMessagePin}>Pin</DropdownMenuItem>
+        <DropdownMenuItem onClick={handleSelectedMessageDelete}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default MessageDropDownOptions
