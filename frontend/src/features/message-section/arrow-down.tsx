e5692b743fc1da2d692a7cdb97496905e3f7c97a
Owais <owaisahmadqureshi019@gmail.com>
2025-03-18 13:53:38 +0500

    feat(ArrowDown) Added arrow down svg

---
+++ frontend/src/features/message-section/arrow-down.tsx
@@ -0,0 +1,16 @@
+function ArrowDown() {
+  return <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M12 15.25a.75.75 0 0 0 .53-.22l4.5-4.5a.75.75 0 1 0-1.06-1.06L12 13.44l-3.97-3.97a.75.75 0 1 0-1.06 1.06l4.5 4.5a.75.75 0 0 0 .53.22z"
+      clipRule="evenodd"
+    />
+  </svg>
+}
+
+export default ArrowDown
