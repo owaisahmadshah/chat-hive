5649140ed402771bf1d7a8a5658d3e4f7a4a4021
Owais <owaisahmadqureshi019@gmail.com>
2025-03-20 01:45:38 +0500

    refac(chatService) updated fetchUser arugment name messageBody to email.

--- frontend/src/features/chat-section/services/chatService.ts
+++ frontend/src/features/chat-section/services/chatService.ts
@@ -1,7 +1,7 @@
 import api from "@/lib/axiosInstance"
 
-const fetchUser = async (messageBody: any, token: string | null) => {
-  const { data } = await api.post("/v1/webhook/suggestions", messageBody, {
+const fetchUser = async (email: any, token: string | null) => {
+  const { data } = await api.post("/v1/webhook/suggestions", email, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
