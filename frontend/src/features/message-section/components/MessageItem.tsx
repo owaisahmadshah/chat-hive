252483932463252e6a5eb4fc25cddd30dc107894
Owais <owaisahmadqureshi019@gmail.com>
2025-04-01 01:37:01 +0500

    feat(message) add download option for images in message item and now messages are easily showing

--- frontend/src/features/message-section/components/MessageItem.tsx
+++ frontend/src/features/message-section/components/MessageItem.tsx
@@ -1,11 +1,42 @@
+import { Download } from "lucide-react"
+
 import correctDate from "@/lib/correct-date"
 import { Message } from "@/features/message-section/types/message-interface"
 import MessageActions from "./MessageActions"
+import { Button } from "@/components/ui/button"
 
 function MessageItem({ message }: { message: Message }) {
-  // TODO complete it by adding download image option or just show on the screen
+  const handleDownload = async () => {
+    if (message.photoUrl.trim() === "") {
+      return
+    }
+    // TODO: Handle download of image
+  }
+  const handleImageClick = () => {
+    // TODO
+  }
+
   return (
     <div className="box-border inline-block m-1">
+      {message.photoUrl.trim() !== "" && (
+        <div className="relative w-[250px] h-[350px] overflow-hidden rounded-lg">
+          <img
+            src={message.photoUrl}
+            alt="Uploaded"
+            loading="lazy"
+            className="w-full h-full object-contain bg-black cursor-pointer"
+            onClick={handleImageClick}
+          />
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute bottom-2 right-2 bg-black/50 text-white rounded-full p-2 hover:bg-black/70 cursor-pointer"
+            onClick={handleDownload}
+          >
+            <Download className="w-5 h-5" />
+          </Button>
+        </div>
+      )}
       <p className="whitespace-pre-wrap break-words w-fit p-2">{message.message}</p>
       <div className="flex justify-between items-center">
         <p className="text-[10px] ml-4">{correctDate(message.updatedAt)}</p>
