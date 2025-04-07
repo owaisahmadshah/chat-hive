750322e9b6fae389d603dcc30e52ce6143aa7d65
Owais <owaisahmadqureshi019@gmail.com>
2025-04-07 20:40:40 +0500

    feat(MessageNavBar) Implement dialog for user profile interaction in MessageNavBar component

--- frontend/src/features/message-section/components/MessageNavBar.tsx
+++ frontend/src/features/message-section/components/MessageNavBar.tsx
@@ -4,6 +4,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { RootState } from "@/store/store"
 import correctDate from "@/lib/correct-date"
 import useUserOnlineStatus from "../hooks/useUserOnlineStatus"
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+
 
 const MessageNavBar = () => {
 
@@ -11,31 +19,42 @@ const MessageNavBar = () => {
 
   useUserOnlineStatus()
 
-  const handleClickOnSelectedChatProfile = () => {
-    // TODO 
-  }
-
   return (
-    <ul className="w-[100%] h-[15vh] flex gap-5 items-center p-5 bg-background border-b border-r">
-      <li className=" cursor-pointer" onClick={handleClickOnSelectedChatProfile}>
-        <Avatar>
-          <AvatarImage src={selectedChatUser?.imageUrl} />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </li>
-      <li onClick={handleClickOnSelectedChatProfile}>
-        <strong className="text-lg cursor-pointer">{selectedChatUser?.email}</strong>
-        <p className="text-sm text-muted-foreground">
-          {
-            selectedChat?.typing && selectedChat?.typing.isTyping
-              ? "typing..." :
-              selectedChatUser?.isUserOnline
-                ? "online"
-                : selectedChatUser?.updatedAt && correctDate(selectedChatUser?.updatedAt)
-          }
-        </p>
-      </li>
-    </ul>
+    <Dialog>
+      <ul className="w-[100%] h-[10vh] pt-2 pl-2 bg-background border-b border-r">
+        <DialogTrigger className="flex gap-5 items-center">
+          <li className="cursor-pointer">
+            <Avatar>
+              <AvatarImage src={selectedChatUser?.imageUrl} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </li>
+          <li className="flex flex-col items-start">
+            <strong className="text-lg cursor-pointer">{selectedChatUser?.email}</strong>
+            <p className="text-xs text-muted-foreground">
+              {
+                selectedChat?.typing && selectedChat?.typing.isTyping
+                  ? "typing..." :
+                  selectedChatUser?.isUserOnline
+                    ? "online"
+                    : selectedChatUser?.updatedAt && correctDate(selectedChatUser?.updatedAt)
+              }
+            </p>
+          </li>
+        </DialogTrigger>
+        <DialogContent className="flex flex-col items-center">
+          <img src={selectedChatUser?.imageUrl} alt=""
+            height="150px"
+            width="150px"
+            className="rounded-full"
+            loading="lazy"
+          />
+          <DialogTitle className="cursor-pointer hover:underline">{selectedChatUser?.email}</DialogTitle>
+          <p className="text-muted-foreground">{selectedChat?.typing?.isTyping ? "Typing..." : selectedChatUser?.isUserOnline ? "Online" : "Offline"}</p>
+          <Button variant={"destructive"} className="cursor-pointer">Delete Chat</Button>
+        </DialogContent>
+      </ul>
+    </Dialog>
   )
 }
 
